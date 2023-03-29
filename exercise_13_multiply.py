63e6cfacb9dd43de11a0fe4eb72d13e6baed2ca8
Faahbio <fab.mat.ski@gmail.com>
2023-03-29 12:41:07 -0300

    first commit

---
+++ exercise_13_multiply.py
@@ -0,0 +1,33 @@
+def dup(number):
+    
+    return (number * 2)
+
+def triple(number):
+    
+    return (number * 3)
+    
+def quadup(number):
+    
+    return (number * 4)
+
+print(dup(2))
+print(triple(4))
+print(quadup(6))
+
+# --------------------------------------------------
+
+def multiply(multiplier):
+    
+    def do_multiply(number):
+        
+        return number * multiplier
+    
+    return do_multiply
+
+dup = multiply(2)
+triple = multiply(3)
+quadup = multiply(4)
+
+print(dup(2))
+print(triple(4))
+print(quadup(6))
